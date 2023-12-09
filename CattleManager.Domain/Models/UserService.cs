de00eaa695bc1b4f75f2e4befeb79262e30cca9c
Natan <nataniel08@protonmail.com>
2023-12-09 17:01:27 -0300

    user service save user

--- CattleManager.Domain/Models/UserService.cs
+++ CattleManager.Domain/Models/UserService.cs
@@ -1,5 +1,25 @@
+using CattleManager.Domain.Interfaces;
+
 namespace CattleManager.Domain.Models;
 
 public class UserService
 {
+    private readonly IRepository<User> _repository;
+
+    public UserService(IRepository<User> repository)
+    {
+        _repository = repository;
+    }
+
+    public void Save(User user)
+    {
+        UserExists(user.Id);
+        _repository.Save(user);
+    }
+
+    private void UserExists(int id)
+    {
+        if (_repository.GetById(id) != null)
+            throw new Exception("User already exists");
+    }
 }
