fb450946cbe1b448418f413d0074bb27613b0500
Natan <nataniel08@protonmail.com>
2023-12-09 17:20:20 -0300

    base repository implemented

---
+++ CattleManager.Infra/Repositories/Repository.cs
@@ -0,0 +1,36 @@
+using CattleManager.Domain.Interfaces;
+using CattleManager.Domain.Models;
+using CattleManager.Infra.Context;
+
+namespace CattleManager.Infra.Repositories;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+{
+    protected readonly AppDbContext _context;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public virtual IEnumerable<TEntity> GetAll()
+    {
+        var query = _context.Set<TEntity>();
+        if (query.Any())
+            return query.ToList();
+        return new List<TEntity>();
+    }
+
+    public virtual TEntity GetById(int id)
+    {
+        var query = _context.Set<TEntity>().Where(x => x.Id == id);
+        if (query.Any())
+            return query.FirstOrDefault();
+        return null;
+    }
+
+    public virtual void Save(TEntity entity)
+    {
+        _context.Set<TEntity>().Add(entity);
+    }
+}
